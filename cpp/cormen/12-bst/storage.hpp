a9a7f493aad516c136faeef83ff019ae6c21b677
Dariusz Donimirski <dariusz.donimirski@gmail.com>
2020-01-07 18:09:35 +0100

    separate id_type

--- cpp/cormen/12-bst/storage.hpp
+++ cpp/cormen/12-bst/storage.hpp
@@ -2,82 +2,9 @@
 #define STORAGE_HPP
 
 #include <iostream>
+#include "id_type.hpp"
 
-//TODO: implement more the 1 flags
-template<class base_type=unsigned, unsigned FLAGS_BITS=1>
-struct id_type_t {
-    using inner_type = base_type;
-    constexpr static inner_type NIL{ static_cast<inner_type>(-1) >> FLAGS_BITS };
-    constexpr static inner_type FLG{ ~NIL };
-    constexpr static inline auto get_id(inner_type id) { return id & NIL; }
-    constexpr static inline auto get_flags(inner_type id) { return id & FLG; }
-    constexpr static inline auto get_flags_as_low(inner_type id) {
-        return (id & FLG) >> (sizeof(base_type)*8 - FLAGS_BITS);
-    }
-
-    inner_type __id;
-
-    // provide non-explict conversion base_type -> id_type_t
-    id_type_t(inner_type id=NIL): __id{get_id(id)} {}
-    // provide non-explict conversion id_type_t -> base_type
-    operator inner_type() const { return (get_id(__id)); }
-
-    id_type_t(id_type_t const&) = default; // copy all
-    //id_type_t(id_type_t&&) = delete;
-
-    // might this oprator should be deleted 
-    id_type_t& operator = (id_type_t const& id) noexcept {
-         __id = get_flags(__id) | get_id(id.__id);
-         return *this;
-    }
-
-    id_type_t operator++ () {
-        ++__id;
-        return *this;
-    }
-
-    void set_NIL() { __id = NIL; };
-
-    //TODO: consider friend inline add to the interface
-    friend inline bool is_on(id_type_t const& id) {
-        return !!get_flags(id.__id);
-    }
-
-    friend inline bool is_NIL(id_type_t const& id) {
-        return get_id(id.__id) == NIL;
-    }
-
-    friend inline id_type_t& on(id_type_t& id) {
-        id.__id |= FLG;
-        return id;
-    }
-
-    friend inline id_type_t& off(id_type_t& id) {
-        id.__id &= NIL;
-        return id;
-    }
-
-    friend inline void copy_id(id_type_t& dst, id_type_t const& src) {
-        dst.__id = get_flags(dst.__id) | get_id(src.__id);
-    }
-
-    friend inline void copy_flags(id_type_t& dst, id_type_t const& src) {
-        dst.__id = get_id(dst.__id) | get_flags(src.__id);
-    }
-
-    friend inline void copy(id_type_t& dst, id_type_t const& src) {
-        dst.__id = src.__id;
-    }
-
-    friend std::ostream& operator<<(std::ostream& os, id_type_t const& n) {
-        if (n.__id == NIL)
-            return os << "NIL";
-        return os << get_id(n.__id);
-    }
-};
-
-using id_type = id_type_t<>;
-
+using id_type = id_ui1f_type;
 
 template<class NODE=unsigned, unsigned N=20>
 struct storage_type {
@@ -98,6 +25,7 @@ struct storage_type {
         NODE node;
     };
 #endif
+
     portion arr[N];
 
     storage_type(): __free{0u} {
@@ -108,7 +36,7 @@ struct storage_type {
     }
 
     id_type alloc() {
-        if (is_NIL(__free))
+        if (__free.is_NIL())
             return id_type{};
         auto const ret = __free;
         __free = arr[__free].next;
@@ -121,7 +49,7 @@ struct storage_type {
     }
 
     bool is_empty() const {
-        return is_NIL(__free);
+        return __free.is_NIL();
     }
 
     NODE& operator[](unsigned id) {
---
+++ cpp/cormen/12-bst/test_id_type.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include "id_type.hpp"
+
+using std::cout, std::endl;
+
+//TODO: write down tests
+
+int main() {
+
+    id_ui0f_type id0;
+    id_ui1f_type id1;
+    id_ui2f_type id2;
+
+    id1.on();
+    id2.on();
+
+    cout << (sizeof(unsigned) * 8)-1 << endl;
+
+    cout << id0 << endl;
+    cout << id1 << endl;
+    cout << id2 << endl;
+
+    id1.off();
+    id2.off();
+
+    id2.set_flag(1);
+    id2.set_flag(0);
+    
+
+    cout << id0 << endl;
+    cout << id1 << ' ' << id1.is_on() << endl;
+    cout << id2 << ' ' << id2.is_on() << id2.is_set(1) << id2.is_set(0) << endl;
+
+    if (id0.is_NIL()) { }
+    if (id1.is_NIL()) { }
+    if (id2.is_NIL()) { }
+
+
+    id2.copy_id(id2);
+
+}
